d0d8213807eb4d2dbb4f31eb137dec195a1779c4
httpjamesm <github@httpjames.space>
2022-04-02 16:04:10 -0400

    basic landing page with inputs

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Router, Route } from "svelte-routing";
+
+    import Landing from "./routes/Landing.svelte";
+
+    export let url: string = "";
+</script>
+
+<Router {url}>
+    <div>
+        <Route path="/"><Landing /></Route>
+    </div>
+</Router>
---
+++ src/routes/Landing.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let username: string = "";
+
+    let roomKey: string = "";
+
+    const doJoin = async () => {
+        window.localStorage.setItem("username", username);
+        window.localStorage.setItem("roomKey", roomKey);
+    };
+</script>
+
+<body>
+    <div class="container">
+        <h1>CryptoChat 3.0</h1>
+        <h2>Simple, secure and ephemeral anonymous messaging.</h2>
+        <input type="text" placeholder="Username" bind:value={username} />
+        <input type="text" placeholder="Room Key" bind:value={roomKey} />
+        <button on:click={doJoin}>Join</button>
+    </div>
+</body>
+
+<style lang="scss">
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .container {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
